a879a9a8b18f34024f5b2ff15bcbec6e5879fdb5
andradebru <brunansandrade@gmail.com>
2022-08-11 15:53:23 -0300

    ex 06 resolvido

--- lista-01/js06-inverter-texto.js
+++ lista-01/js06-inverter-texto.js
@@ -1,16 +1,46 @@
 /**
- * Faça um Programa que inverta um texto. 
- * 
+ * Faça um Programa que inverta um texto.
+ *
  * ENTRADA          SAÍDA
  * abacate          etacaba
  * 12345            54321
- * 
+ *
  */
 
 // Entrada
-var entrada = 'abacate'
+var entrada = "abacate";
+
+// MODO 1
+// function inverteString(str) {
+//   var splitString = str.split(""); //separa as letras da string
+//   var arrayInvertida = splitString.reverse(); //inverte
+//   var juntaArray = arrayInvertida.join(""); //junta tudo de novo numa nova string
+//   return juntaArray; //retorna a nova string
+// }
 
 // gere a variável invertido de forma a transforma a entrada na saída esperada
-var invertido = '?'
+// var invertido = inverteString(entrada);
 
-console.log(invertido)
+/* MODO 2
+function inverteString(str) {
+  return str.split("").reverse().join("");
+}
+var invertido = inverteString(entrada);
+*/
+
+// /* MODO 3
+var novaString = "";
+for (var letra = entrada.length - 1; letra >= 0; letra--) {
+  novaString += entrada[letra];
+}
+var invertido = novaString;
+// */
+
+// /* MODO 3 alt
+// var novaString = "";
+// entrada.forEach(function (letra) {
+//   novaString += letra;
+// });
+// */
+
+console.log(invertido);
