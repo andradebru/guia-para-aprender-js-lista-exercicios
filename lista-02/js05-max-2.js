1ac470a60dd2a445dfda2fcb8a2d30ed665b8f11
andradebru <brunansandrade@gmail.com>
2022-08-12 13:51:33 -0300

    exercicio 08 resolvido

--- lista-02/js05-max-2.js
+++ lista-02/js05-max-2.js
@@ -1,20 +1,30 @@
 /**
- * Faça um programa que mostre o segundo maior número da lista 
- * 
+ * Faça um programa que mostre o segundo maior número da lista
+ *
  * ENTRADA              SAÍDA
  * [1,3,5,7,8,10,3]      8
  * [1,1,40]              1
  * [40,42,1,0,-10]      40
- * 
+ *
  * IMPORTANTE:
  * Não usar sort
  * Não usar funções built-ins da linguagem
  */
 
 // Entrada
-var entrada = [40,42,1,0,-10]
+var entrada = [40, 42, 1, 0, -10];
+var ordenada = [];
+var maior = 0;
 
-var segundoMaior = 40
+for (let index = 0; index < entrada.length; index++) {
+  const element = entrada[index];
+  if (element > maior) {
+    ordenada.push(element);
+    console.log(ordenada);
+  }
+  //   element > maior ? (maior = element) : maior;
+  //   element > maior ? (ordenada.push(entrada[index]), (maior = element)) : maior;
+}
 
-
-console.log(segundoMaior)
+var segundoMaior = ordenada.pop(-1);
+console.log(segundoMaior);
--- lista-02/js08-impar-count.js
+++ lista-02/js08-impar-count.js
@@ -1,20 +1,32 @@
 /**
  * Percorra a entrada com uma lista de nros e retorne qtos numeros impares/pares
- * 
+ *
  * ENTRADA:
  * [1,2,3,4,5,6,7]
- * 
+ *
  * SAIDA
  * 4 3
  */
 
 // Entrada
-var entrada = [1,2,3,4,5,6,7]
+var entrada = [1, 2, 3, 4, 5, 6, 7];
+var impar = [];
+var par = [];
 
 // processamento
+entrada.forEach(function (element) {
+  if (element % 2 == 0) {
+    par.push(element);
+  } else {
+    impar.push(element);
+  }
+});
+
+console.log(par);
+console.log(impar);
 
 //saida
-var impares = 4,
-    pares = 3
+var impares = impar.length,
+  pares = par.length;
 
-console.log(`${impares} ${pares}`)
+console.log(`${impares} ${pares}`);
