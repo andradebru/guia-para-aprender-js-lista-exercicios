24a4601ccad512ab692c9809cb2a677a281f335e
andradebru <brunansandrade@gmail.com>
2022-08-12 11:39:07 -0300

    logica ex 01

--- lista-02/js01-impar-par.js
+++ lista-02/js01-impar-par.js
@@ -1,17 +1,20 @@
 /**
  * Faça um programa que percorra uma lista e gere um outra lista com o
- * mesmo numero de elementos no formato: 'impar' ou 'par' 
- * 
+ * mesmo numero de elementos no formato: 'impar' ou 'par'
+ *
  * ENTRADA          SAÍDA
  * [1,3,5,7,8]      ['impar', 'impar', 'impar', 'impar', 'par']
  */
 
 // Entrada
-var entrada = [1,3,5,7,8]
-
-var saida = []
+var entrada = [1, 3, 5, 7, 8];
+var saida = "";
+entrada.forEach(function (elemento) {
+  var parimpar = elemento % 2 == 0 ? "'par'" : "'impar'";
+  saida += parimpar + ", ";
+});
 
 // gerar a lista de saida
-saida = ['impar', 'impar', 'impar', 'impar', 'par']
+// saida = ['impar', 'impar', 'impar', 'impar', 'par']
 
-console.log(saida)
+console.log(saida);
