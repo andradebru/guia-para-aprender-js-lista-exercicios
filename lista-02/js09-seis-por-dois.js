0c6f98c2b03d8ab765d45712ef47c4f8c6d57dad
andradebru <brunansandrade@gmail.com>
2022-08-12 16:04:53 -0300

    ex 09-02 resolvido

--- lista-02/js09-seis-por-dois.js
+++ lista-02/js09-seis-por-dois.js
@@ -5,10 +5,33 @@
  * trabalhando ou de folga. Difícil fazer planos.
  * Exemplo, como saber no dia 12 de Outubro ele vai estar trabalhando ou de folga?
  * Como fazer um programa que liste os próximos 3 meses com dias de trabalho/folga?
- * 
+ *
  */
 
 // Entrada
+var dia = new Date();
+var diasParaFolgar = 5;
+var meses = 3;
+
+dia.setDate(dia.getDate() + diasParaFolgar);
+console.log(dia.toLocaleDateString());
+dia.setDate(dia.getDate() + 1);
+console.log(dia.toLocaleDateString());
+
+for (x = 0; x < meses * 4; x++) {
+  console.log("\nDias trabalhados:");
+  for (i = 0; i < 6; i++) {
+    dia.setDate(dia.getDate() + 1);
+    console.log(dia.toLocaleDateString());
+  }
+
+  console.log("\nDias de folga:");
+  for (i = 0; i < 2; i++) {
+    dia.setDate(dia.getDate() + 1);
+    console.log(dia.toLocaleDateString());
+  }
+}
+
 // Pŕoxima volta
 
 // Saída com base na próxima folga, calcular dia de trabalho/folga
