e0c8856290c3849ef6bbd3233374327c0ac9927a
andradebru <brunansandrade@gmail.com>
2022-08-12 11:46:14 -0300

    ex 03 lista 2

--- lista-02/js03-media-lista.js
+++ lista-02/js03-media-lista.js
@@ -1,18 +1,20 @@
 /**
  * Faça um programa que faça a soma de todos os números de uma lista de números
- * 
+ *
  * ENTRADA              SAÍDA
  * [1,3,5,7,8,10]        5.7
  * [1,1,40]             14.0
  */
 
 // Entrada
-var entrada = [1,1,40]
+var entrada = [1, 1, 40];
 
-var soma = 0
+var soma = 0;
 
 // percorrer a lista e fazer a somatória dos elementos
+entrada.forEach(function (elemento) {
+  soma += elemento;
+});
+let media = soma / entrada.length;
 
-let media = 0
-
-console.log(media.toFixed(1))
+console.log(media.toFixed(1));
